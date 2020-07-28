dfa0d3fab6be346a8ec847311909ab2b30401072
canloveme <1792363241@qq.com>
2020-07-28 09:38:07 +0800

    javase02

---
+++ day10-studydemo-Collections/src/cn/csq/test/Test01.java
@@ -0,0 +1,14 @@
+package cn.csq.test;
+
+import java.util.Collection;
+
+/**
+ * @ClassName : Test01
+ * @Author : CSQ
+ * @Date: 2020-07-28 08:22
+ */
+public class Test01 {
+    public static void main(String[] args) {
+
+    }
+}
